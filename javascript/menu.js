3b993c833f23bdff53fedea2ad9c19979f944e9d
JM-2323 <judemoorhouse@gmail.com>
2025-03-11 14:16:57 +0000

    Added start page +  menu function, with corresponding css and also javascript (for interactive menu design)

---
+++ javascript/menu.js
@@ -0,0 +1,19 @@
+// Toggle menu
+function toggleMenu() {
+    var menuColumn = document.getElementById("menuColumn");
+    menuColumn.classList.toggle("active");
+}
+
+// Show submenu
+function showSubMenu(submenuId) {
+    var submenu = document.getElementById(submenuId);
+    submenu.classList.add("active");
+}
+
+// Close submenu
+function closeSubMenu() {
+    var submenus = document.querySelectorAll(".submenu-column");
+    submenus.forEach(function(submenu) {
+        submenu.classList.remove("active");
+    });
+}
